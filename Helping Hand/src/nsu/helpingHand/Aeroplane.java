c1efb017f6fe1c18b380085de4c55bbded566c88
FahimMuntashir <fahim.tech.me@gmail.com>
2021-04-17 20:31:13 +0600

    ...

--- Helping Hand/src/nsu/helpingHand/Aeroplane.java
+++ Helping Hand/src/nsu/helpingHand/Aeroplane.java
@@ -1,5 +1,44 @@
 package nsu.helpingHand;
 
 public class Aeroplane {
+    private String model;
+    private int capacity;
+    private String  pilot;
+
+    public Aeroplane() {
+        this.model = "Boeing";
+        this.capacity = 60;
+    }
+
+    public Aeroplane(String model, int capacity, String pilot) {
+        this.model = model;
+        this.capacity = capacity;
+        this.pilot = pilot
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getPilot() {
+        return pilot;
+    }
+
+    public void setPilot(String pilot) {
+        this.pilot = pilot;
+    }
+
 
 }
