df2e7160712d51883ff9e010ba92a38d095ef726
FahimMuntashir <fahim.tech.me@gmail.com>
2021-05-10 18:04:01 +0600

    added...

--- Lab_11/src/nsu/lab11/Task_1.java
+++ Lab_11/src/nsu/lab11/Task_1.java
@@ -1,5 +1,32 @@
 package nsu.lab11;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task_1 {
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("enter two integers: ");
+        int sum = 0;
+        boolean decision = false;
+        do {
+            try {
+                int a = input.nextInt();
+                int b = input.nextInt();
+                sum = a + b;
+                System.out.println("sum = " + sum);
+                decision = false;
+            } catch (InputMismatchException e) {
+                System.out.println(e);
+                System.out.print("enter integers again: ");
+                decision = true;
+                input.nextLine();
+
+            }
+        } while (decision);
+
+
+    }
 }
---
+++ Lab_11/src/nsu/lab11/Task_2.java
@@ -0,0 +1,7 @@
+package nsu.lab11;
+
+public class Task_2 {
+    public static void main(String[] args) {
+
+    }
+}
