eb99d2509fe878f5a81f5386960d275686d8a4a8
FahimMuntashir <fahim.tech.me@gmail.com>
2021-05-10 18:18:00 +0600

    added...

--- Lab_11/src/nsu/lab11/Task_2.java
+++ Lab_11/src/nsu/lab11/Task_2.java
@@ -1,7 +1,27 @@
 package nsu.lab11;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task_2 {
     public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+            int[] array = new int[100];
 
+        for (int i = 0; i < array.length; i++) {
+            array[i] =(int) ((Math.random()*100)+1);
+        }
+
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i]+" ");
+//        }
+
+        System.out.print("enter the index of the array: ");
+        int value  = input.nextInt();
+        try{
+            System.out.println(array[value]);
+        }catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("Out of Bounds.");
+        }
     }
 }
---
+++ Lab_11/src/nsu/lab11/Task_3.java
@@ -0,0 +1,7 @@
+package nsu.lab11;
+
+public class Task_3 {
+    public static void main(String[] args) {
+
+    }
+}
