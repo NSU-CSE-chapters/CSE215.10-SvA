9c6c98aeebd7940a940982307ef4bac0d6f9536e
FahimMuntashir <fahim.tech.me@gmail.com>
2021-05-22 23:37:44 +0600

    Info Form

---
+++ Helping Hand/src/nsu/helpingHand/Hammer.java
@@ -0,0 +1,24 @@
+package nsu.helpingHand;
+
+public class Hammer extends Tool{
+    private double power;
+
+    public Hammer(int length, double power) {
+        super(length);
+        this.power = power;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+
+    @Override
+    public String display() {
+        return super.display()+", power = "+getPower();
+    }
+}
